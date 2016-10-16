8d65bc4af0f283323ab2d0a260863d4facb58c35
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-10-15 23:44:38 -0500

    test get /movie

--- test/movies.js
+++ test/movies.js
@@ -4,7 +4,7 @@ const host = api.server;
 const request = supertest(host);
 
 describe('Movie route', () => {
-  describe('Request to post', () => {
+  describe('POST /movie', () => {
     it('should create a movie', (done) => {
       let movie = {
         title: 'back to the future',
@@ -26,5 +26,63 @@ describe('Movie route', () => {
         done(err);
       });
     })
+  });
+
+  describe('GET /movie', () => {
+    it('should return all movies', (done) => {
+      let movie_id, movie2_id;
+      let movie = {
+        title: 'back to the future',
+        year: '1985'
+      };
+      let movie2 = {
+        title: 'back to the future II',
+        year: '1989'
+      };
+
+      request
+        .post('/movie')
+        .set('Accept', 'application/json')
+        .send(movie)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+      .then((res) => {
+        const movie_id = res.body.movie._id;
+        return request
+          .post('/movie')
+          .set('Accept', 'application/json')
+          .send(movie2)
+          .expect(201)
+          .expect('Content-Type', /application\/json/)
+      })
+      .then((res) => {
+        const movie2_id = res.body.movie._id;
+        return request
+          .get('/movie')
+          .set('Accept', 'application/json')
+          .expect(200)
+          .expect('Content-Type', /application\/json/)
+      }, done)
+      .then((res) => {
+        const {body} = res;
+        expect(body).to.have.property('movies');
+        expect(body.movies).to.be.an('array')
+          .and.to.have.length.above(2);
+
+        const {movies} = body;
+        const movie = movies.filter((mov) => mov._id === movie_id)[0];
+        const movie2 = movies.filter((mov) => mov._id === movie2_id)[0];
+
+        expect(movie).to.have.property('_id', movie_id);
+        expect(movie).to.have.property('title', 'back to the future');
+        expect(movie).to.have.property('year', '1985');
+
+        expect(movie2).to.have.property('_id', movie_id);
+        expect(movie2).to.have.property('title', 'back to the future II');
+        expect(movie2).to.have.property('year', '1989');
+
+        done();
+      }, done);
+    });
   });
 });
