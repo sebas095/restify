9e321351460ea8f9915625337474bc5ee0c8ce2f
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-10-16 02:26:05 -0500

    test post /user

---
+++ test/user.js
@@ -0,0 +1,42 @@
+const supertest = require('supertest');
+const api = require('../app');
+const host = api.server;
+const request = supertest(host);
+const mongoose = require('mongoose');
+const config = require('../config/test');
+
+describe('User route', () => {
+  before(() => {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(config.db.url);
+  });
+
+  after((done) => {
+    mongoose.disconnect(done);
+    mongoose.models = {};
+  });
+
+  describe('POST /user', () => {
+    it.only('should create a new user', (done) => {
+      const user = {
+        username: 'sebas095',
+        password: 'secret123'
+      };
+
+      request
+        .post('/user')
+        .send(user)
+        .set('Accept', 'application/json')
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+      .end((err, res) => {
+        const {body} = res;
+        expect(body).to.have.property('user');
+        expect(body.user).to.have.property('username', 'sebas095');
+        expect(body.user).to.have.property('password');
+        expect(body.user).to.have.property('_id');
+        done(err);
+      });
+    });
+  });
+});
